717b805b7bbac17e97a07f21f2e27d8f5328032a
poschuler <poschuler@gmail.com>
2024-09-28 15:40:54 -0500

    remove dotenv

--- app/db/db.server.ts
+++ app/db/db.server.ts
@@ -1,7 +1,6 @@
 import { drizzle } from "drizzle-orm/libsql";
-import { createClient } from "@libsql/client/http";
+import { createClient } from "@libsql/client";
 import * as schema from "./scheme/schema.server";
-import 'dotenv/config';
 
 if (!process.env.TURSO_DATABASE_URL) {
   throw new Error("Missing environment variable: TURSO_DATABASE_URL");
