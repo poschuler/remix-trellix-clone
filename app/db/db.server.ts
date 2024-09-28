da9b0d3c6849f662f63ecb453f1942e0c69f9144
poschuler <poschuler@gmail.com>
2024-09-28 15:35:06 -0500

    dotenv

--- app/db/db.server.ts
+++ app/db/db.server.ts
@@ -1,6 +1,7 @@
 import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client/http";
 import * as schema from "./scheme/schema.server";
+import 'dotenv/config';
 
 if (!process.env.TURSO_DATABASE_URL) {
   throw new Error("Missing environment variable: TURSO_DATABASE_URL");
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,6 @@
 import type { Config } from "drizzle-kit";
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 export default {
   schema: "./app/db/scheme/schema.server.ts",
