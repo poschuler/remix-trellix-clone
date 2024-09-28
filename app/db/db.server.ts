f6c8bd0a2eb9549cde356746c7aa07532c635f09
poschuler <poschuler@gmail.com>
2024-09-28 15:25:08 -0500

    add env variables validation

--- app/db/db.server.ts
+++ app/db/db.server.ts
@@ -2,6 +2,14 @@ import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
 import * as schema from "./scheme/schema.server";
 
+if (!process.env.TURSO_DATABASE_URL) {
+  throw new Error("Missing environment variable: TURSO_DATABASE_URL");
+}
+
+if (!process.env.TURSO_AUTH_TOKEN) {
+  throw new Error("Missing environment variable: TURSO_AUTH_TOKEN");
+}
+
 const turso = createClient({
   url: process.env.TURSO_DATABASE_URL!,
   authToken: process.env.TURSO_AUTH_TOKEN,
