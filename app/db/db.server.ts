256c356a9eb7ab3a61e7388c49a5eb49906c92a8
poschuler <poschuler@gmail.com>
2024-09-28 16:34:36 -0500

    deploy vercel

--- app/db/db.server.ts
+++ app/db/db.server.ts
@@ -3,7 +3,7 @@ import { createClient } from "@libsql/client";
 import * as schema from "./scheme/schema.server";
 
 if (!process.env.TURSO_DATABASE_URL) {
-  throw new Error("Missing environment variable: TURSO_DATABASE_URL");
+  throw new Error("Missing environment variable: TURSO_DATABASE_URL ");
 }
 
 if (!process.env.TURSO_AUTH_TOKEN) {
