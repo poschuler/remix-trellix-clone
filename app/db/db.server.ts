eeb0f793ed82828d7c706dd15d1076d1efb13495
poschuler <poschuler@gmail.com>
2024-09-28 16:21:04 -0500

    VITE env

--- app/db/db.server.ts
+++ app/db/db.server.ts
@@ -2,17 +2,17 @@ import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
 import * as schema from "./scheme/schema.server";
 
-if (!process.env.TURSO_DATABASE_URL) {
-  throw new Error("Missing environment variable: TURSO_DATABASE_URL");
+if (!process.env.VITE_TURSO_DATABASE_URL) {
+  throw new Error("Missing environment variable: VITE_TURSO_DATABASE_URL");
 }
 
-if (!process.env.TURSO_AUTH_TOKEN) {
+if (!process.env.VITE_TURSO_AUTH_TOKEN) {
   throw new Error("Missing environment variable: TURSO_AUTH_TOKEN");
 }
 
 const turso = createClient({
-  url: process.env.TURSO_DATABASE_URL!,
-  authToken: process.env.TURSO_AUTH_TOKEN,
+  url: process.env.VITE_TURSO_DATABASE_URL!,
+  authToken: process.env.VITE_TURSO_AUTH_TOKEN,
 });
 
 export const db = drizzle(turso, { schema });
